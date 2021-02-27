9ad1d5d733d9d12ffd42489152e298341fedfc6b
Apoorv Vardhman <apoorv.vardhman@gmail.com>
2021-02-27 14:40:47 +0530

    27 Feb

---
+++ app/src/main/java/com/apoorv/activtyexample/ui/Test.java
@@ -0,0 +1,13 @@
+package com.apoorv.activtyexample.ui;
+
+/**
+ * Created by Apoorv Vardhman on 2/27/2021
+ *
+ * @Email :  apoorv.vardhman@gmail.com
+ * @Author :  Apoorv Vardhman (developerapoorv.xyz)
+ * @Linkedin :  https://in.linkedin.com/in/apoorv-vardhman
+ * @Skype :  apoorv.vardhman
+ * Contact :  +91 8434014444
+ */
+public class Test {
+}
